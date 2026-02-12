3c88a228bbbc2e77cf458ccd9893665b0e953f32
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-12 07:58:43 +0000

    refactor: update wifi powersave rules

--- install/config/wifi-powersave-rules.sh
+++ install/config/wifi-powersave-rules.sh
@@ -1,7 +1,7 @@
 if omarchy-battery-present; then
   cat <<EOF | sudo tee "/etc/udev/rules.d/99-wifi-powersave.rules"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="$HOME/.local/share/omarchy/bin/omarchy-wifi-powersave on"
-SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="$HOME/.local/share/omarchy/bin/omarchy-wifi-powersave off"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="0", RUN+="$HOME/.local/share/omarchy/bin/hiarchy-wifi-powersave on"
+SUBSYSTEM=="power_supply", ATTR{type}=="Mains", ATTR{online}=="1", RUN+="$HOME/.local/share/omarchy/bin/hiarchy-wifi-powersave off"
 EOF
 
   sudo udevadm control --reload
