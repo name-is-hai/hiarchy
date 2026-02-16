66f0e4106efe7c2595a34e43799c3bfcc5d3caa1
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-17 02:40:56 +0700

    Update base.sh

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,5 +1,5 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$HIARCHY_INSTALL/hiarchy-base.packages" | grep -v '^$')
-yay -S --noconfirm --needed "${packages[@]}"
+yay -Sc --noconfirm --needed "${packages[@]}"
 mapfile -t packages < <(grep -v '^#' "$HIARCHY_INSTALL/hiarchy-other.packages" | grep -v '^$')
-yay -S --noconfirm --needed "${packages[@]}"
+yay -Sc --noconfirm --needed "${packages[@]}"
