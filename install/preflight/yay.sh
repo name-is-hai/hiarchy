7ba61b1986a24bdfe6bb9f63ba8458342a82d90e
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-17 07:56:37 +0700

    Update yay.sh

--- install/preflight/yay.sh
+++ install/preflight/yay.sh
@@ -1,11 +1,17 @@
-# Move to a temporary directory (like /opt or your home folder)
+# Exit immediately if yay is already installed
+if command -v yay >/dev/null 2>&1; then
+  echo "yay is already installed. Nothing to do."
+  exit 0
+fi
+
+# Move to home directory
 cd ~/
 
 # Clone the repository
 git clone https://aur.archlinux.org/yay.git
 
 # Change ownership so your user can build it (makepkg cannot be run as root)
-sudo chown -R $(whoami):$(whoami) ./yay
+sudo chown -R "$(whoami)":"$(whoami)" ./yay
 
 # Navigate into the folder
 cd yay
