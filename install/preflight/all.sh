283a1873055bd69f927d8fb7c16fdd7aadd4e9f9
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-19 07:07:20 +0000

    fix: update step when run first-run-mode.sh

--- install/preflight/all.sh
+++ install/preflight/all.sh
@@ -1,8 +1,8 @@
 source $HIARCHY_INSTALL/preflight/guard.sh
 source $HIARCHY_INSTALL/preflight/begin.sh
 run_logged $HIARCHY_INSTALL/preflight/show-env.sh
-run_logged $HIARCHY_INSTALL/preflight/yay.sh
 run_logged $HIARCHY_INSTALL/preflight/pacman.sh
 run_logged $HIARCHY_INSTALL/preflight/migrations.sh
 run_logged $HIARCHY_INSTALL/preflight/first-run-mode.sh
 run_logged $HIARCHY_INSTALL/preflight/disable-mkinitcpio.sh
+run_logged $HIARCHY_INSTALL/preflight/yay.sh
