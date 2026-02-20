4409c22a0cfae0862bc2bce8727759e4eaf58f35
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-20 11:23:03 +0000

    fix: update to yay install

--- install/packaging/base.sh
+++ install/packaging/base.sh
@@ -1,6 +1,3 @@
 # Install all base packages
 mapfile -t packages < <(grep -v '^#' "$HIARCHY_INSTALL/hiarchy-base.packages" | grep -v '^$')
 yay -S --noconfirm --needed "${packages[@]}"
-
-# mapfile -t packages < <(grep -v '^#' "$HIARCHY_INSTALL/hiarchy-other.packages" | grep -v '^$')
-# yay -S --noconfirm --needed "${packages[@]}"
