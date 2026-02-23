96daf629cba7188f6dc1b1ae32f12c54ea0059bc
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-02-23 16:32:55 +0000

    feat: update install messager

--- install/packaging/webapps.sh
+++ install/packaging/webapps.sh
@@ -1,2 +1,3 @@
 hiarchy-webapp-install "YouTube" https://youtube.com/ YouTube.png
 hiarchy-webapp-install "Zalo" https://chat.zalo.me/ Zalo.png
+hiarchy-webapp-install "Messager" https://www.messenger.com/ Messager.png
