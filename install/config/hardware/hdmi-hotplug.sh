a79fa29525e6d076f3e0014b8b8caa3df91da086
name-is-hai <haiqd065@gmail.com>
2026-02-28 12:30:49 +0700

    fix: enhance monitor detection script and update HDMI hotplug handling

--- install/config/hardware/hdmi-hotplug.sh
+++ install/config/hardware/hdmi-hotplug.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
 cat <<EOF | sudo tee "/etc/udev/rules.d/99-hdmi-hotplug.rules"
 ACTION=="change", SUBSYSTEM=="drm", ENV{HOTPLUG}=="1", \
 RUN+="/bin/sh -c 'if [ ! -e /tmp/hdmi.lock ]; then \
 touch /tmp/hdmi.lock; \
 (sleep 3; rm /tmp/hdmi.lock) & \
-$HOME/.local/share/hiarchy/bin/hiarchy-main-monitor-detect & \
+su - $USER -c \"$HOME/.local/share/hiarchy/bin/hiarchy-main-monitor-detect\" & \
 fi'"
 EOF
 
