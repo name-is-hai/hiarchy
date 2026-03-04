204ab16ae3e6cd75708b558aeaff27c2e723e6c1
name-is-hai <haiqd065@gmail.com>
2026-03-04 19:42:42 +0700

    refactor: update config file and source config file

--- install/first-run/welcome.sh
+++ install/first-run/welcome.sh
@@ -1,1 +1,1 @@
-notify-send "    Learn Keybindings" "Super + K for cheatsheet.\nSuper + Space for application launcher.\nSuper + Alt + Space for Hiarchy Menu." -u critical
+notify-send "    Learn Keybindings" "Super + K for cheatsheet.\nAlt + Space for application launcher.\nSuper + Alt + Space for Hiarchy Menu." -u critical
