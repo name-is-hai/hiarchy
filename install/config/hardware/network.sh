00daaec3a8ad29ee7c532a83678ef307647f90c2
Vũ Đào Ngọc Hải <90458368+name-is-hai@users.noreply.github.com>
2026-03-05 13:32:55 +0000

    feat: move from impala, systemd-networkd to NetworkManager, gazelle

--- install/config/hardware/network.sh
+++ install/config/hardware/network.sh
@@ -1,6 +1,24 @@
 # Ensure iwd service will be started
 sudo systemctl enable iwd.service
 
+# Configure NetworkManager
+sudo mkdir -p /etc/NetworkManager/conf.d
+sudo ln -snf "$HIARCHY_PATH/default/networkmanager/hiarchy_networkmanager.conf" "/etc/NetworkManager/conf.d/hiarchy_networkmanager.conf"
+
 # Prevent systemd-networkd-wait-online timeout on boot
 sudo systemctl disable systemd-networkd-wait-online.service
 sudo systemctl mask systemd-networkd-wait-online.service
+
+# Mask WPA Supplicant to prevent it from being started by NetworkManager
+sudo systemctl mask --now wpa_supplicant.service
+
+# Mask systemd-networkd and its sockets to prevent it from being started
+sudo systemctl mask --now systemd-networkd.socket
+sudo systemctl mask --now systemd-networkd-varlink.socket
+sudo systemctl mask --now systemd-networkd.service
+
+# Enable and start NetworkManager
+sudo systemctl enable NetworkManager.service
+
+# Prevent NetworkManager-wait-online timeout on boot
+sudo systemctl mask NetworkManager-wait-online.service
