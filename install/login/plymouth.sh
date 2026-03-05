64eca15e26954171b52ccb7095e89834d11bdc1e
name-is-hai <90458368+name-is-hai@users.noreply.github.com>
2026-03-05 07:28:12 +0700

    fix: update plymouth themes directory

--- install/login/plymouth.sh
+++ install/login/plymouth.sh
@@ -1,4 +1,5 @@
 if [ "$(plymouth-set-default-theme)" != "hiarchy" ]; then
+  sudo mkdir -p /usr/share/plymouth/themes
   sudo ln -snf "$HIARCHY_PATH/default/plymouth/" "/usr/share/plymouth/themes/hiarchy/"
   sudo plymouth-set-default-theme hiarchy
 fi
